6b8ff015364ca040881a07949c4e2d76a99082a8
Nadilla C. Putri <58943422+nadillachantika@users.noreply.github.com>
2024-09-21 15:07:10 +0700

    layouting dashboard

---
+++ resources/views/landingpage/layouts/footer.blade.php
@@ -0,0 +1,3 @@
+<footer>
+    <p>copyright 2024</p>
+</footer>
---
+++ resources/views/landingpage/profile.blade.php
@@ -0,0 +1,12 @@
+@extends('landingpage.layouts.master')
+
+
+@section('content')
+
+
+    <h1>Ini Profile</h1>
+    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Amet quasi ab corporis reiciendis, soluta nisi itaque. Culpa
+        iste perferendis alias eaque consequatur commodi vel libero, impedit magni dolores praesentium ipsam.</p>
+@endsection
+
+
